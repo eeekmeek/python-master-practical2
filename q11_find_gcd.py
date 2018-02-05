9e207215ce960f82bb14aff344dbda5780b38c7e
Nicky Ren <ren.nicky@dhs.sg>
2018-02-05 21:47:14 +0800

    Update q11_find_gcd.py

--- q11_find_gcd.py
+++ q11_find_gcd.py
@@ -1,16 +1,12 @@
 # Computing the greatest common divisor
 
-def gcd(n1, n2):
-    gcd = 1
-    
-    if n1 % n2 == 0:
-        return n2
-    
-    for k in range(int(n2 / 2), 0, -1):
-        if n1 % n2 == 0 and n2 % k == 0:
-            gcd = k
-            break  
-    return gcd
-
-print(gcd(12, 17))
-print(gcd(4, 6))
+def gcd(n1,n2):
+    if(n2==0):
+        return n1
+    else:
+        return gcd(n2,n1%n2)
+n1=int(input("Enter first integer:"))
+n2=int(input("Enter second integer:"))
+GCD=gcd(n1,n2)
+print("GCD is: ")
+print(GCD)
